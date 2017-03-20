8f42228ddc17822771e295e6201bbd94a60031e9
Maxime FRANC <mfranc@e2r8p16.42.fr>
2017-03-20 20:10:01 +0100

    segv on free find, add more condition in lastdatasmanager after a conversion

--- ft_printf/ft_last_datasmanager.c
+++ ft_printf/ft_last_datasmanager.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 11:32:05 by mfranc            #+#    #+#             */
-/*   Updated: 2017/03/17 11:47:18 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/03/20 20:09:07 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_last_datasmanager(t_datas *datas, char conv, char **lastdatas)
 {
-	if (datas->len == 0 && datas->cplen == 0)
+	if (datas->len == 0 && datas->cplen == 0 && (conv == 'n' || conv == 'w'))
 		datas->result = NULL;
 	if (datas->result && (conv != 'n' || conv != 'w'))
 		ft_strdel(lastdatas);
