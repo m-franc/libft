9ba7be4e3c1b4e8927019c4207873c0abe95ff34
Maxime FRANC <mfranc@e2r8p16.42.fr>
2017-03-23 19:28:21 +0100

    wide string precision is almost ok, one precision 0 test, and its good

--- ft_printf/convs/ls/ft_ls_precision.c
+++ ft_printf/convs/ls/ft_ls_precision.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 12:36:58 by mfranc            #+#    #+#             */
-/*   Updated: 2017/03/22 15:13:13 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/03/23 19:27:44 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,13 @@ int			ft_ls_precision(char **argcvd, t_flags *flags)
 	char	*tmp;
 
 	if (flags->precision < 0
-		|| flags->precision >= (int)ft_strlen(*argcvd))
+			|| flags->precision >= (int)ft_strlen(*argcvd))
 		return (0);
-//	while (!(argcvd[0][flags->precision] & 0xF0)
-//			|| !(argcvd[0][flags->precision] & 0xE0)
-//			|| !(argcvd[0][flags->precision] & 0xC0))
-//	while (argcvd[0][flags->precision] & 0x80)
-//	{
-//		PSTR("LOCTET : ")
-//		ft_putbits(&argcvd[0][flags->precision], 1);
-//		ENDL
-//		flags->precision -= 1;
-//	}
+	while ((argcvd[0][flags->precision] & 0x40) == 0
+			|| (argcvd[0][flags->precision] & 0xc0) == 0)
+		flags->precision -= 1;
 	if (!(tmp = ft_strsub(*argcvd, 0, flags->precision)))
-		return (-1);
+		return (-1);	
 	ft_strdel(argcvd);
 	*argcvd = tmp;
 	return (1);
